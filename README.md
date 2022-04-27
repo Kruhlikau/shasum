2811a6244a7733792bebc12e90f04aaefc1488af
Kruhlikau Aleh <aleh.kruhlikau@mmc.com>
2022-04-27 13:35:47 +0300

    v1_shasum for file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# shasum
+# Python script for calculating hash sum
+____
+**To start using the script, enter the following command in the console:**
+**`python3 shasum.py [file_name] [hash_algorithm]`**
+
+where:
+* ### file_name - file for hash calculation
+* ### hash_algorithm - hashing algorithms for a file:
+    **md4, sha512, sha512_224, sha224, shake_128, sha384, md5, sha3_256, md5-sha1, sha3_224, sha512_256, sm3, sha3_384, blake2s,sha3_512, sha1, sha256, ripemd160, whirlpool, blake2b, shake_256**
+
---
+++ shasum.py
@@ -0,0 +1,19 @@
+import argparse
+import hashlib
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file_name', type=str,
+                    help='file for hash calculation')
+parser.add_argument('hash_algorithm', type=str, default='md5',
+                    help=f'hashing algorithms for a file: {", ".join(hashlib.algorithms_available)}')
+args = parser.parse_args()
+
+
+def file_hash_sum(file_name, hash_algorithm):
+    with open(file_name, 'rb') as f:
+        h = hashlib.new(hash_algorithm)
+        h.update(f.read())
+    return h.hexdigest()
+
+
+print(file_hash_sum(file_name=args.file_name, hash_algorithm=args.hash_algorithm))
