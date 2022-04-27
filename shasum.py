d80462bde3906f1d74a4058801bf578ad956cb68
Kruhlikau Aleh <akruhlikau@sncsoft.com>
2022-04-27 17:41:26 +0300

    updated file_hash_sum func

--- shasum.py
+++ shasum.py
@@ -13,7 +13,16 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def file_hash_sum(file_name, hash_algorithm):
+def file_hash_sum(file_name: str, hash_algorithm: str) -> str:
+    """
+    The function takes a file and a hashing algorithm as arguments,
+     returns the hash sum
+
+    :rtype: str
+    :param file_name: file for hash calculation
+    :param hash_algorithm: hashing algorithms for a file:
+    :return: hash sum of the file
+    """
     with open(file_name, "rb") as f:
         h = hashlib.new(hash_algorithm)
         h.update(f.read())
